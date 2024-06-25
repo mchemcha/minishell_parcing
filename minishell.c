37178af242c629e90df2b11eddbdebcda84dd3cf
Maryem Chemchaq <mchemcha@e3r3p1.1337.ma>
2024-06-25 16:22:29 +0100

    saloua 9biha

--- minishell.c
+++ minishell.c
@@ -6,49 +6,32 @@
 /*   By: mchemcha <mchemcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 19:12:59 by mchemcha          #+#    #+#             */
-/*   Updated: 2024/06/22 20:51:25 by mchemcha         ###   ########.fr       */
+/*   Updated: 2024/06/24 14:20:43 by mchemcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// int main()
-// {
-//     char *str;
-//     char **tab;
-//     int i;
-//     t_token *list = NULL;
-
-//     char *read_line;
-//     while(1)
-//     {
-//         i = 0;
-//         read_line = readline("minishell : ");
-//         add_history(read_line);
-//         double_quot_error(read_line);
-//         betwen_quote(&read_line);
-//         str = space(read_line);
-//         // printf("%s\n",str);
-//         tab = ft_split(str, ' ');
-//         while(tab[i])
-//         {
-//             tokenazer_line(tab[i], &list);
-//             i++;
-//         }
-//         print_stack(list);
-//         list = NULL;
-//     }
-// }
-
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
-    char *read_line;
-    while(1)
-    {
-        read_line = readline("minishell : ");
-        add_history(read_line);
-        parcing_check(read_line);
-    }
+	char	*read_line;
+	
+	(void)ac;
+	(void)av;
+	while(1)
+	{
+		read_line = readline("minishell: ");
+		if (!read_line)
+		{
+			printf("\n");
+			continue;
+		}
+		if (strcmp(read_line, "") != 0)
+		{
+			add_history(read_line);
+			parcing_check(read_line);
+		}
+		free(read_line);
+	}
+	return 0;
 }
